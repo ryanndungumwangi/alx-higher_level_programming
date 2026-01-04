7d880855e12524978091bf1e93821ac8b420f0f4
ryanndungumwangi <ryan.ndungumwangi@gmail.com>
2026-01-04 18:05:03 +0300

    Use format to print them out

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(100):
     if i < 10:
-        print(f"0{i}, ", end="")
+        print("0{}, ".format(i), end="")
     else:
-        print(i, end=", " if i < 99 else "")
+        print("{}".format(i), end=", " if i < 99 else "")
 print()
